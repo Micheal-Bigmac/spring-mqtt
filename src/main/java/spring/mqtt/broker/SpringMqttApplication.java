08d13d1a323bdb23f637790566fa4953ed9813ef
Administrator <Administrator@DESKTOP-N108ODN>
2017-07-06 09:41:08 +0800

    创建项目

---
+++ src/main/java/spring/mqtt/broker/SpringMqttApplication.java
@@ -0,0 +1,12 @@
+package spring.mqtt.broker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMqttApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMqttApplication.class, args);
+	}
+}
