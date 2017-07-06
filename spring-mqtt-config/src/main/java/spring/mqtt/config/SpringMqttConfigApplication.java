5503cc93dbde15ef088a2122908e3060ce7938fb
xiaocui1 <895175533@qq.com>
2017-07-06 10:44:43 +0800

    添加配置项目

---
+++ spring-mqtt-config/src/main/java/spring/mqtt/config/SpringMqttConfigApplication.java
@@ -0,0 +1,12 @@
+package spring.mqtt.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMqttConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMqttConfigApplication.class, args);
+	}
+}
