569efccbf8744dd3216386dadd2db623e418b7ff
xiaocui1 <895175533@qq.com>
2017-07-06 14:40:23 +0800

    基础通信配置

--- spring-mqtt-config/src/main/java/spring/mqtt/config/SpringMqttConfigApplication.java
+++ spring-mqtt-config/src/main/java/spring/mqtt/config/SpringMqttConfigApplication.java
@@ -1,12 +1,21 @@
 package spring.mqtt.config;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringMqttConfigApplication {
+@EnableAutoConfiguration
+public class SpringMqttConfigApplication  implements CommandLineRunner{
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringMqttConfigApplication.class, args);
+	}
+
+	@Override
+	public void run(String... strings) throws Exception {
+
 	}
 }
